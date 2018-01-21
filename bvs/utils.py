8ffd44cc2627e8c0904dee9e52eaa92f72118990
Abdulla P I <hereabdulla@gmail.com>
2018-01-21 15:01:49 +0530

    realigned checks group,updated profile & utils

--- bvs/utils.py
+++ bvs/utils.py
@@ -18,9 +18,39 @@ def get_group_checks(checks_group):
     cg = frappe.get_doc("Checks Group", checks_group)
     checklist = []
     if cg.employment_check:
-        checklist.append('employment')
-    if cg.education_check:
-        checklist.append('education')
+        checklist.append('employment1')
     if cg.second_employment:
-        checklist.append('employment2')    
+        checklist.append('employment2')
+    if cg.third_employment:
+        checklist.append('employment3')
+    if cg.education_check:
+        checklist.append('education1')
+    if cg.second_education:
+        checklist.append('education2')
+    if cg.third_education:
+        checklist.append('education3')
+    if cg.reference_check:
+        checklist.append('reference1')
+    if cg.second_reference:
+        checklist.append('reference2')
+    if cg.third_reference:
+        checklist.append('reference3')
+    if cg.identity_check:
+        checklist.append('aadhar')
+    if cg.driving_license_verification:
+        checklist.append('driving')
+    if cg.voter_id_verification:
+        checklist.append('voter')
+    if cg.pan_card_verification:
+        checklist.append('pan')
+    if cg.passport_verification:
+        checklist.append('passport')
+    if cg.family_detail_check:
+        checklist.append('family')
+    if cg.criminal_check:
+        checklist.append('criminal')
+    if cg.court_record_check:
+        checklist.append('court')
+    if cg.address_check:
+        checklist.append('address')
     return checklist
