1979653e8f931e419a400a2f7fdec214a932a0cf
VHRS <abdulla.pi@voltechgroup.com>
2018-10-19 21:15:24 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-import re, ast
+import re
+import ast
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in bvs/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -10,17 +13,14 @@ with open('bvs/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
-
 setup(
-	name='bvs',
-	version=version,
-	description='VHRS Background Verification Service',
-	author='VHRS',
-	author_email='erp@voltechgroup.com',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name='bvs',
+    version=version,
+    description='VHRS Background Verification Service',
+    author='VHRS',
+    author_email='erp@voltechgroup.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
 )
